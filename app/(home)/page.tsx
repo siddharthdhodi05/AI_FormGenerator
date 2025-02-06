e02f781c0d0b499263fa16e246341b85abb233cf
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2025-02-07 01:44:37 +0530

    Feature: Implement the UI of HomePage.

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/HeroSection";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className=" grid items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ components/PricingPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PricingPage = () => {
+  return <div></div>;
+};
+
+export default PricingPage;
